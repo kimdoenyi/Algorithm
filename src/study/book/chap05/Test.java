804cc376f70d111dd9002be9ccde3f86045d2568
김진성 <kjs4082@naver.com>
2022-04-19 22:32:30 +0900

    알고리즘 예제

---
+++ src/study/book/chap05/Test.java
@@ -0,0 +1,16 @@
+package study.book.chap05;
+
+public class Test {
+
+    public static void main(String[] args) {
+        
+        String[] data = {"bat", "eat", "ate", "long", "kong"};
+
+        for(int i=0; i<data.length-1; i++) {   // 반복 횟수를 위한 반복문
+            for(int j=0; j<data.length-1-i; j++) {     // swap 을 위한 반복문
+
+            }
+        }
+                
+    }
+}
